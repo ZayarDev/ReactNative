4b475f6e3a25f996536a3b24763a1f584df743cc
Zayar <zayartun16@gmail.com>
2019-11-27 11:25:00 +0630

    Added Container into ScrollView

--- App.js
+++ App.js
@@ -7,7 +7,15 @@
  */
 
 import React, {Component} from 'react';
-import {SafeAreaView, ScrollView, View, Text, StyleSheet, Image, FlatList} from 'react-native';
+import {
+  SafeAreaView,
+  ScrollView,
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  FlatList,
+} from 'react-native';
 
 const data = [
   {
@@ -68,9 +76,13 @@ class App extends Component {
       navigationBarView: {
         flex: 0.06,
         backgroundColor: '#5BCFF7',
+        justifyContent: 'center',
+      },
+      scrollView: {
+        flex: 0.94,
       },
       upperView: {
-        flex: 0.4,
+        flex: 0.13,
         backgroundColor: 'white',
       },
       subUpperView: {
@@ -84,69 +96,143 @@ class App extends Component {
         borderRadius: 20,
       },
       lowerView: {
-        flex: 0.54,
+        flex: 0.25,
         backgroundColor: 'white',
       },
     });
     return (
-      <SafeAreaView style={{flex: 1, backgroundColor: '#5BCFF7'}}>
-        <View style={style.container}>
-          <View style={style.navigationBarView}>
-            <View style={{flex: 1, flexDirection: 'row-reverse', alignContent: 'center'}}>
-              <View style={{flex: 0.45, alignSelf: 'center', marginBottom: 10, marginRight: 15}}>
-                <Image
-                  style={{width: 20, height: 20, alignSelf: 'flex-end'}}
-                  source={require('./icons/search.png')}
-                />
-              </View>
-              <View style={{alignSelf: 'center', marginBottom: 10}}>
-                <Text style={{fontSize: 16, fontWeight: 'bold', color: 'white'}}>
-                  Search City
-                </Text>
-              </View>
+      <View style={{flex: 1}}>
+        <SafeAreaView style={{flex: 0, backgroundColor: '#5BCFF7'}} />
+        <SafeAreaView style={{flex: 1, backgroundColor: 'white'}}>
+          <View style={style.container}>
+            <View style={style.navigationBarView}>
+              <Text
+                style={{
+                  fontSize: 16,
+                  fontWeight: 'bold',
+                  color: 'white',
+                  alignSelf: 'center',
+                }}>
+                Search City
+              </Text>
+              <Image
+                style={{width: 20, height: 20, position: 'absolute', right: 15}}
+                source={require('./icons/search.png')}
+              />
             </View>
-          </View>
-          <View style={style.upperView}>
-            <View style={style.subUpperView}>
-              <View style={{flex: 0.1, justifyContent: 'center', marginTop: 10}}>
-                <Text style={{alignSelf: 'center', fontSize: 14, fontWeight: 'bold', color: 'white'}}> Current Weather</Text>
-              </View>
-              <View style={{flex: 0.3, justifyContent: 'center', marginTop: 10}}>
-                <Text style={{alignSelf: 'center', fontSize: 50, fontWeight: 'bold', color: 'white'}}> 23 °C</Text>
-              </View>
-              <View style={{flex: 0.1, justifyContent: 'center', marginTop: 10}}>
-                <Text style={{alignSelf: 'center', fontSize: 16, fontWeight: 'bold', color: 'white'}}> Few Clouds</Text>
-              </View>
-              <View style={{flex: 0.5, flexDirection: 'row', marginTop: 5}}>
-                <View style={{flex: 0.6, justifyContent: 'center'}}>
-                  <Text style={{alignSelf: 'flex-start', marginLeft: 20, fontSize: 13, fontWeight: 'bold', color: 'white'}}> Humidity 83%</Text>
-                  <Text style={{alignSelf: 'flex-start', marginLeft: 20, marginTop: 7, fontSize: 13, fontWeight: 'bold', color: 'white'}}> Wind 1km/hr</Text>
+            <ScrollView
+              style={style.scrollView}
+              contentContainerStyle={{flexGrow: 1}}>
+              <View style={style.upperView}>
+                <View style={style.subUpperView}>
+                  <View
+                    style={{flex: 0.1, justifyContent: 'center', marginTop: 7}}>
+                    <Text
+                      style={{
+                        alignSelf: 'center',
+                        fontSize: 14,
+                        fontWeight: 'bold',
+                        color: 'white',
+                      }}>
+                      {' '}
+                      Current Weather
+                    </Text>
+                  </View>
+                  <View
+                    style={{flex: 0.1, justifyContent: 'center', marginTop: 5}}>
+                    <Text
+                      style={{
+                        alignSelf: 'center',
+                        fontSize: 50,
+                        fontWeight: 'bold',
+                        color: 'white',
+                      }}>
+                      {' '}
+                      23 °C
+                    </Text>
+                  </View>
+                  <View
+                    style={{flex: 0.1, justifyContent: 'center', marginTop: 5}}>
+                    <Text
+                      style={{
+                        alignSelf: 'center',
+                        fontSize: 16,
+                        fontWeight: 'bold',
+                        color: 'white',
+                      }}>
+                      {' '}
+                      Few Clouds
+                    </Text>
+                  </View>
+                  <View
+                    style={{
+                      flex: 0.7,
+                      flexDirection: 'row',
+                      marginTop: 5,
+                      marginBottom: 10,
+                    }}>
+                    <View style={{flex: 0.6, justifyContent: 'center'}}>
+                      <Text
+                        style={{
+                          alignSelf: 'flex-start',
+                          marginLeft: 20,
+                          fontSize: 13,
+                          fontWeight: 'bold',
+                          color: 'white',
+                        }}>
+                        {' '}
+                        Humidity 83%
+                      </Text>
+                      <Text
+                        style={{
+                          alignSelf: 'flex-start',
+                          marginLeft: 20,
+                          marginTop: 7,
+                          fontSize: 13,
+                          fontWeight: 'bold',
+                          color: 'white',
+                        }}>
+                        {' '}
+                        Wind 1km/hr
+                      </Text>
+                    </View>
+                    <View style={{flex: 0.4, justifyContent: 'center'}}>
+                      <Image
+                        style={{width: 50, height: 50, alignSelf: 'center'}}
+                        source={require('./icons/cloud.png')}
+                      />
+                    </View>
+                  </View>
                 </View>
-                <View style={{flex: 0.4, justifyContent: 'center'}}>
-                  <Image
-                    style={{width: 50, height: 50, alignSelf: 'center'}}
-                    source={require('./icons/cloud.png')}
+              </View>
+              <View style={style.lowerView}>
+                <View style={{justifyContent: 'center', marginTop: 10}}>
+                  <Text
+                    style={{
+                      alignSelf: 'center',
+                      fontSize: 14,
+                      fontWeight: 'bold',
+                      color: 'black',
+                    }}>
+                    {' '}
+                    Focus 5 days weather
+                  </Text>
+                </View>
+                <View style={{flex: 0.6, marginLeft: 15, marginTop: 20}}>
+                  <FlatList
+                    horizontal={true}
+                    data={this.state.data}
+                    renderItem={({item: rowData}) => {
+                      return <FocusWeatherCard item={rowData} />;
+                    }}
+                    keyExtractor={(item, index) => index}
                   />
                 </View>
               </View>
-            </View>
+            </ScrollView>
           </View>
-          <View style={style.lowerView}>
-            <View style={{justifyContent: 'center', marginTop: 10}}>
-              <Text style={{alignSelf: 'center', fontSize: 14, fontWeight: 'bold', color: 'black'}}> Focus 5 days weather</Text>
-            </View>
-            <View style={{flex: 0.6, marginLeft: 15, marginTop: 20}}>
-              <FlatList horizontal={true}
-                data={this.state.data}
-                renderItem={({item: rowData}) => {
-                  return <FocusWeatherCard item={rowData} />;
-                }}
-                keyExtractor={(item, index) => index}
-              />
-            </View>
-          </View>
-        </View>
-      </SafeAreaView>
+        </SafeAreaView>
+      </View>
     );
   }
 }
@@ -164,20 +250,69 @@ function FocusWeatherCard({item}) {
   return (
     <View style={[style.container, {backgroundColor: item.color}]}>
       <View style={{flex: 0.2, justifyContent: 'center', marginTop: 5}}>
-        <Text style={{alignSelf: 'center', fontSize: 14, fontWeight: 'bold', color: 'white'}}> {item.day}</Text>
+        <Text
+          style={{
+            alignSelf: 'center',
+            fontSize: 14,
+            fontWeight: 'bold',
+            color: 'white',
+          }}>
+          {' '}
+          {item.day}
+        </Text>
       </View>
       <View style={{flex: 0.35, justifyContent: 'center', marginTop: 5}}>
-        <Image style={{width: 50, height: 60, alignSelf: 'center'}} source={item.icon}
+        <Image
+          style={{width: 50, height: 60, alignSelf: 'center'}}
+          source={item.icon}
         />
       </View>
-      <View style={{flex: 0.1, justifyContent: 'center', marginTop: 5}}>
-        <Text style={{alignSelf: 'center', fontSize: 14, fontWeight: 'bold', color: 'white'}}> {item.temperature}</Text>
+      <View
+        style={{
+          flex: 0.1,
+          justifyContent: 'center',
+          marginTop: 5,
+        }}>
+        <Text
+          style={{
+            alignSelf: 'center',
+            fontSize: 14,
+            fontWeight: 'bold',
+            color: 'white',
+          }}>
+          {' '}
+          {item.temperature}
+        </Text>
       </View>
       <View style={{flex: 0.1, justifyContent: 'center', marginTop: 5}}>
-        <Text style={{alignSelf: 'center', fontSize: 13, fontWeight: 'bold', color: 'white'}}> {item.temperatureMiddle}</Text>
+        <Text
+          style={{
+            alignSelf: 'center',
+            fontSize: 13,
+            fontWeight: 'bold',
+            color: 'white',
+          }}>
+          {' '}
+          {item.temperatureMiddle}
+        </Text>
       </View>
-      <View style={{flex: 0.25, justifyContent: 'flex-end', marginTop: 5, marginBottom: 10}}>
-        <Text style={{alignSelf: 'center', fontSize: 12, fontWeight: 'bold', color: 'white'}}> {item.temperatureLower}</Text>
+      <View
+        style={{
+          flex: 0.25,
+          justifyContent: 'flex-end',
+          marginTop: 5,
+          marginBottom: 10,
+        }}>
+        <Text
+          style={{
+            alignSelf: 'center',
+            fontSize: 12,
+            fontWeight: 'bold',
+            color: 'white',
+          }}>
+          {' '}
+          {item.temperatureLower}
+        </Text>
       </View>
     </View>
   );
